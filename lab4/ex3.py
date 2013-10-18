a05cfbfb549534a2f29d5ccdc8f56513a22e4d3a
Roma Duban <romaduban@gmail.com>
2013-10-18 13:25:32 +0300

    lab 4 ex 20.3

---
+++ lab4/ex3.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+import sys
+
+suma = 0
+for x in xrange(1, 6):
+    print x
+    k = 0
+    while k < 5000:
+        if k == 0:
+            k += 1
+            continue
+        fk=k
+        for i in range(1,k):
+            fk *= i
+        suma += ((-1)**(k+1)*x**(2*k+1))/(fk*(2*k+1))
+        k +=1
+        sys.stdout.flush()
+        sys.stdout.write('\r Progress: %s %%' % str(k/50.0))
+print suma
