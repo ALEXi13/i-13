90065fb05cacb456a1566edec0b9917dabd2e468
innka <Sakura8Haruno@yandex.ru>
2013-11-01 15:28:56 +0200

    .

--- лабораторна робота 4/лаб4завд5(2сп).py
+++
@@ -1,7 +0,0 @@
-# *-* coding: utf-8-*-*
-#лабоzerna=input("Введите вагу одного зерна")
-zerna=input("Введите вагу одного зерна")
-zerna2=zerna
-suma1=0
-suma2=zerna2*(1-2**64)/(1-2)
-print "suma vseh zeren", suma2
--- лабораторна робота 4/лаб4№4.py
+++
@@ -1,9 +0,0 @@
-# *-* coding: utf-8-*-*
-# Лабораторна 4 завдання 4
-i=10
-while i>0:
-    print i,"green bottles hanging on the wall,"
-    print i,"green bottles hanging on the wall,"
-    print "And if one green bottle should accidentally fall,"
-    i-=1
-    print "There’ll be", i, "green bottles hanging on the wall."
